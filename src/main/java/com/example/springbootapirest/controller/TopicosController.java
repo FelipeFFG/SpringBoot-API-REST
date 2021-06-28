29cb74c64419fc0d163f84ddce3193d288cbce9f
Felipe <felipeffggomes@hotmail.com>
2021-06-28 18:08:12 -0300

    Adicionando Conteudo de Utilizando Repository

--- src/main/java/com/example/springbootapirest/controller/TopicosController.java
+++ src/main/java/com/example/springbootapirest/controller/TopicosController.java
@@ -1,22 +1,26 @@
 package com.example.springbootapirest.controller;
 
 import com.example.springbootapirest.dto.TopicoDto;
-import com.example.springbootapirest.model.Curso;
 import com.example.springbootapirest.model.Topico;
+import com.example.springbootapirest.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
                                                      //Por padrao ele assume que todos os metodos tem o @ReponseBody.
 @RestController                            //ResponseBody - indica que o retorno do metodo deve ser serializado e devolvido no corpo da resposnta.
 public class TopicosController {
 
+
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDto> lista(){
-        Topico topico = new Topico("Dúvida","Dúvida com Spring",new Curso("Spring","Programação"));
-        return TopicoDto.converter(Arrays.asList(topico,topico,topico));
+        List<Topico> topicos = topicoRepository.findAll();
+        return TopicoDto.converter(topicos);
     }
 
 }
--- src/main/java/com/example/springbootapirest/repository/TopicoRepository.java
+++ src/main/java/com/example/springbootapirest/repository/TopicoRepository.java
@@ -3,7 +3,6 @@ package com.example.springbootapirest.repository;
 import com.example.springbootapirest.model.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface TopicoRepository extends JpaRepository<Topico,Long> {
 
 }
