a68ad84bf27ffc2992b82bc10225763fc78f96f4
Felipe <felipeffggomes@hotmail.com>
2021-06-28 16:01:38 -0300

    Adicionando conteudo de Usando DTO

--- src/main/java/com/example/springbootapirest/controller/TopicosController.java
+++ src/main/java/com/example/springbootapirest/controller/TopicosController.java
@@ -1,23 +1,24 @@
 package com.example.springbootapirest.controller;
 
+import com.example.springbootapirest.controller.dto.TopicoDto;
 import com.example.springbootapirest.model.Curso;
 import com.example.springbootapirest.model.Topico;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+                                                     //Por padrao ele assume que todos os metodos tem o @ReponseBody.
+@RestController                            //ResponseBody - indica que o retorno do metodo deve ser serializado e devolvido no corpo da resposnta.
 public class TopicosController {
 
-
     @RequestMapping("/topicos")
-    @ResponseBody
-    public List<Topico> lista(){
-        Topico topico = new Topico("Duvida","Duvida com Spring",new Curso("Spring","Programação"));
-        return Arrays.asList(topico,topico,topico);
+    public List<TopicoDto> lista(){
+        Topico topico = new Topico("Dúvida","Dúvida com Spring",new Curso("Spring","Programação"));
+        return TopicoDto.converter(Arrays.asList(topico,topico,topico));
     }
 
 }
