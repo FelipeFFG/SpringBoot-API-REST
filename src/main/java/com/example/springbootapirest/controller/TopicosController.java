8b72e8ad12ff79325322cb9448e0dee1a9668a9b
Felipe <felipeffggomes@hotmail.com>
2021-06-29 10:55:57 -0300

    Adicionando Conteudo de Cadastrando Topicos

--- src/main/java/com/example/springbootapirest/controller/TopicosController.java
+++ src/main/java/com/example/springbootapirest/controller/TopicosController.java
@@ -1,23 +1,28 @@
 package com.example.springbootapirest.controller;
 
 import com.example.springbootapirest.dto.TopicoDto;
+import com.example.springbootapirest.form.TopicoForm;
 import com.example.springbootapirest.model.Topico;
+import com.example.springbootapirest.repository.CursoRepository;
 import com.example.springbootapirest.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
                                                      //Por padrao ele assume que todos os metodos tem o @ReponseBody.
 @RestController                            //ResponseBody - indica que o retorno do metodo deve ser serializado e devolvido no corpo da resposnta.
+@RequestMapping("/topicos")
 public class TopicosController {
 
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @GetMapping
     public List<TopicoDto> lista(String nomeCurso){
         if (nomeCurso==null){
             List<Topico> topicos = topicoRepository.findAll();
@@ -26,6 +31,14 @@ public class TopicosController {
             List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);  //filtra os topicos que tiverem o nome do curso igual o passado na url.
             return TopicoDto.converter(topicos);
         }
+    }
+
+
+    @PostMapping
+    public void cadastrar(@RequestBody TopicoForm form ){  //sinaliza pra pegar no corpo da requisição
+        Topico topico =form.converter(cursoRepository);    //chama o repositorio do curso, pra buscar o curso com aquele nome, e converte os dados em um objeto TOPICO
+        topicoRepository.save(topico);                     //salvamos o TOPICO no banco de dados.
+        
     }
 
 }
