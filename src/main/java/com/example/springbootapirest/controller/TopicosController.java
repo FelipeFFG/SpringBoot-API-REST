8fb4ecda3cf0e9b5b82432530ff1e3ca7efa40ef
Felipe <felipeffggomes@hotmail.com>
2021-06-29 13:40:06 -0300

    Finalizando o projeto

--- src/main/java/com/example/springbootapirest/controller/TopicosController.java
+++ src/main/java/com/example/springbootapirest/controller/TopicosController.java
@@ -2,20 +2,23 @@ package com.example.springbootapirest.controller;
 
 import com.example.springbootapirest.dto.DetalhesDoTopicoDto;
 import com.example.springbootapirest.dto.TopicoDto;
+import com.example.springbootapirest.form.AtualizacaoTopicoForm;
 import com.example.springbootapirest.form.TopicoForm;
 import com.example.springbootapirest.model.Topico;
 import com.example.springbootapirest.repository.CursoRepository;
 import com.example.springbootapirest.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
-                                                     //Por padrao ele assume que todos os metodos tem o @ReponseBody.
+//Por padrao ele assume que todos os metodos tem o @ReponseBody.
 @RestController                            //ResponseBody - indica que o retorno do metodo deve ser serializado e devolvido no corpo da resposnta.
 @RequestMapping("/topicos")
 public class TopicosController {
@@ -40,6 +43,7 @@ public class TopicosController {
 
 
     @PostMapping
+    @Transactional
     public ResponseEntity<TopicoDto> cadastrar(@Valid @RequestBody TopicoForm form, UriComponentsBuilder uriBuilder){  //sinaliza pra pegar no corpo da requisição
 
         Topico topico =form.converter(cursoRepository);    //chama o repositorio do curso, pra buscar o curso com aquele nome, e converte os dados em um objeto TOPICO
@@ -51,9 +55,37 @@ public class TopicosController {
 
 
     @GetMapping("/{id}")
-    public DetalhesDoTopicoDto detalhar(@PathVariable Long id){
-        Topico topico = topicoRepository.getOne(id);
-        return new DetalhesDoTopicoDto(topico);
+    public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id){
+        Optional<Topico> topico = topicoRepository.findById(id);
+        if (topico.isPresent()){
+            return  ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));
+        }
+        return ResponseEntity.notFound().build();
+
     }
 
+
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id,@Valid @RequestBody AtualizacaoTopicoForm form){   //atualiza no  banco de dados automaticamente
+        Optional<Topico> optional = topicoRepository.findById(id);
+        if (optional.isPresent()){
+            Topico topico = form.atualizar(id,topicoRepository);
+            return ResponseEntity.ok(new TopicoDto(topico));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id){
+        Optional<Topico> optional = topicoRepository.findById(id);
+        if (optional.isPresent()){
+            topicoRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
